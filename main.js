a9290882624663f4c0da9c2a37a90a17620b452d
sulaiman <mdsulaiman132k@gmail.com>
2021-04-27 03:48:58 +0530

    second Update

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 let player1 = new GamePlayer(1, -100);
 let player2 = new GamePlayer(2, 850);
+let play;
+
+function playAudio(filename) {
+  var audio = new Audio("audio/" + filename + ".mp3");
+  audio.play();
+}
 
 player1.loadimages((images) => {
   let queueAnimation = [];
@@ -26,6 +32,10 @@ player1.loadimages((images) => {
         aux
       );
 
+    if (selectedAnimation != "idle" || opponentAnimation != "oppidle") {
+      playAudio(play);
+    }
+
     player1.steps +=
       selectedAnimation == "forward" && player1.steps < player2.steps - 50
         ? 100
@@ -50,56 +60,89 @@ player1.loadimages((images) => {
   aux();
   $("#kick").on("click", function () {
     queueAnimation.push("kick");
+    play = "punch";
   });
   $("#block").on("click", function () {
     queueAnimation.push("block");
+    play = "block";
   });
   $("#punch").on("click", function () {
     queueAnimation.push("punch");
+    play = "punch";
   });
   $("#backward").on("click", function () {
     queueAnimation.push("backward");
+    play = "walk";
   });
   $("#forward").on("click", function () {
     queueAnimation.push("forward");
+    play = "walk";
   });
   $("#oppkick").on("click", function () {
     oppqueueAnimation.push("oppkick");
+    play = "punch";
   });
   $("#oppblock").on("click", function () {
     oppqueueAnimation.push("oppblock");
+    play = "block";
   });
   $("#opppunch").on("click", function () {
     oppqueueAnimation.push("opppunch");
+    play = "punch";
   });
   $("#oppbackward").on("click", function () {
     oppqueueAnimation.push("oppforward");
+    play = "walk";
   });
   $("#oppforward").on("click", function () {
     oppqueueAnimation.push("oppbackward");
+    play = "walk";
   });
   document.addEventListener("keyup", (event) => {
     const key = event.key;
-    if (key == "a") {
-      queueAnimation.push("punch");
-    } else if (key == "d") {
-      queueAnimation.push("kick");
-    } else if (key == "w") {
-      queueAnimation.push("block");
-    } else if (key == "x") {
-      queueAnimation.push("forward");
-    } else if (key == "z") {
-      queueAnimation.push("backward");
-    } else if (key == "ArrowLeft") {
-      oppqueueAnimation.push("oppforward");
-    } else if (key == "ArrowRight") {
-      oppqueueAnimation.push("oppbackward");
-    } else if (key == "k") {
-      oppqueueAnimation.push("oppkick");
-    } else if (key == "p") {
-      oppqueueAnimation.push("opppunch");
-    } else if (key == "i") {
-      oppqueueAnimation.push("oppblock");
+    switch (key) {
+      case "a":
+        queueAnimation.push("punch");
+        play = "punch";
+        break;
+      case "d":
+        queueAnimation.push("kick");
+        play = "punch";
+        break;
+      case "w":
+        queueAnimation.push("block");
+        play = "block";
+        break;
+      case "x":
+        queueAnimation.push("forward");
+        play = "walk";
+        break;
+      case "z":
+        queueAnimation.push("backward");
+        play = "walk";
+        break;
+      case "ArrowLeft":
+        oppqueueAnimation.push("oppforward");
+        play = "walk";
+        break;
+      case "ArrowRight":
+        oppqueueAnimation.push("oppbackward");
+        play = "walk";
+        break;
+      case "k":
+        oppqueueAnimation.push("oppkick");
+        play = "punch";
+        break;
+      case "p":
+        oppqueueAnimation.push("opppunch");
+        play = "punch";
+        break;
+      case "i":
+        oppqueueAnimation.push("oppblock");
+        play = "block";
+        break;
+      default:
+        break;
     }
   });
 });
